d2755c9422f894b3d698ca8bc729671362de2bab
Dinh18 <152268268+Dinh18@users.noreply.github.com>
2024-04-24 03:17:35 +0700

    Update EnemyManager.java

--- src/entities/EnemyManager.java
+++ src/entities/EnemyManager.java
@@ -13,6 +13,7 @@ public class EnemyManager {
 
 	private Playing playing;
 	private BufferedImage[][] pigArr;
+	private BufferedImage[][] kingPigArr;
 	private Level currentLevel;
 
 	public EnemyManager(Playing playing) {
@@ -26,23 +27,32 @@ public class EnemyManager {
 
 	public void update(int[][] lvlData) {
 		boolean isAnyActive = false;
-		for (Pig p : currentLevel.getPigs())
+		for (Pig p : currentLevel.getPigs()){
 			if (p.isActive()) {
 				p.update(lvlData, playing);
 				isAnyActive = true;
 			}
+		}
 
+		for(KingPig k : currentLevel.getKings()){
+			if(k.isActive()){
+				k.update(lvlData, playing);
+				isAnyActive = true;
+			}
+		}
+		
 		if (!isAnyActive)
 			playing.setLevelCompleted(true);
 	}
 
 	public void draw(Graphics g, int xLvlOffset) {
 		drawCrabs(g, xLvlOffset);
+		drawKingPig(g, xLvlOffset);
 	}
 
 
 	private void drawCrabs(Graphics g, int xLvlOffset) {
-		for (Pig p : currentLevel.getPigs())
+		for (Pig p : currentLevel.getPigs()){
 			if (p.isActive()) {
 
 				g.drawImage(pigArr[p.getState()][p.getAniIndex()], (int) p.getHitbox().x - xLvlOffset - PIG_DRAWOFFSET_X + p.flipX(),
@@ -51,22 +61,44 @@ public class EnemyManager {
 				p.drawHitbox(g, xLvlOffset);
 				p.drawAttackBox(g, xLvlOffset);
 			}
+		}
+	}
 
+	private void drawKingPig(Graphics g, int xLvlOffset){
+		for(KingPig k : currentLevel.getKings()){
+			if(k.isActive()){
+				g.drawImage(kingPigArr[k.getState()][k.getAniIndex()], (int) k.getHitbox().x - xLvlOffset - KINGPIG_DRAWOFFSET_X + k.flipX(), 
+				(int) k.getHitbox().y - KINGPIG_DRAWOFFSET_Y + (int) k.getPushDrawOffset(), KINGPIG_WIDTH * k.flipW() ,KINGPIG_HEIGHT, null);
+				k.drawHitbox(g, xLvlOffset);
+				k.drawAttackBox(g, xLvlOffset);
+			}
+		}
 	}
 
 	public void checkEnemyHit(Rectangle2D.Float attackBox) {
-		for (Pig p : currentLevel.getPigs())
+		for (Pig p : currentLevel.getPigs()){
 			if (p.isActive())
 				if (p.getState() != DEAD && p.getState() != HIT)
 					if (attackBox.intersects(p.getHitbox())) {
-						p.hurt(10);
+						p.hurt(100);
 						return;
 					}
+		}
+
+		for(KingPig k : currentLevel.getKings()){
+			if(k.isActive())
+				if(k.getState() != DEAD && k.getState() != HIT)
+					if(attackBox.intersects(k.getHitbox())){
+						k.hurt(5);
+						return;
+					}
+		}
 
 	}
 
 	private void loadEnemyImgs() {
 		pigArr = getImgArr(LoadSave.GetSpriteAtlas(LoadSave.PIG_SPRITE), 11, 8, PIG_WIDTH_DEFAULT, PIG_HEIGHT_DEFAULT);
+		kingPigArr = getImgArr(LoadSave.GetSpriteAtlas(LoadSave.KINGPIG_SPRITE), 12, 8, KINGPIG_WIDTH_DEFAULT, KINGPIG_HEIGHT_DEFAULT);
 	}
 
 	private BufferedImage[][] getImgArr(BufferedImage atlas, int xSize, int ySize, int spriteW, int spriteH) {
@@ -80,6 +112,9 @@ public class EnemyManager {
 	public void resetAllEnemies() {
 		for (Pig p : currentLevel.getPigs())
 			p.resetEnemy();
+		for(KingPig k : currentLevel.getKings()){
+			k.resetEnemy();
+		}
 	}
 
 }
